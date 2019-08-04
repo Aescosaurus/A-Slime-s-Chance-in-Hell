9c6f2e327bbc7a3ba1b015d2c672286805aee339
Aescosaurus <michaelbuslik@hotmail.com>
2019-08-03 20:20:49 -0700

    Added lining to progress bar.

--- Engine/ProgressBar.cpp
+++ Engine/ProgressBar.cpp
@@ -1,10 +1,13 @@
 #include "ProgressBar.h"
+#include "SpriteEffect.h"
 
 void ProgressBar::Draw( const Vei2& pos,float percent,
 	Color c,const std::string& text,Graphics& gfx ) const
 {
 	if( percent > 0.0f )
 	{
+		gfx.DrawSprite( pos.x - width / 2 - 3,pos.y - 3,
+			*lining,false,SpriteEffect::Chroma{} );
 		gfx.DrawRectSafe( pos.x - width / 2,pos.y,
 			int( float( width ) * percent ),height,c );
 
--- Engine/ProgressBar.h
+++ Engine/ProgressBar.h
@@ -10,7 +10,8 @@ public:
 	void Draw( const Vei2& pos,float percent,
 		Color c,const std::string& text,Graphics& gfx ) const;
 private:
-	static constexpr int width = 100;
-	static constexpr int height = 20;
+	static constexpr int width = 34 * 3 - 6;
+	static constexpr int height = 7 * 3 - 6;
 	CFontPtr luckyPixel = FontCodex::Fetch( "Fonts/LuckyPixel6x9.bmp" );
+	CSurfPtr lining = SurfCodex::Fetch( "Images/ProgressBar.bmp" );
 };
